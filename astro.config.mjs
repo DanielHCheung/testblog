3074da9aca896661266460f2803d3f790d0af8f8
Daniel Cheung <DanielHCheung@outlook.com>
2026-06-23 19:26:21 -0400

    Update site URL to include trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://danieldata.com",
+	site: "https://danieldata.com/",
 	base: "/testblog",
 	trailingSlash: "always",
 	integrations: [
