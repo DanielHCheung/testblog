a0a47bbcd603bc63c87346be3e8b6587e5533494
Daniel Cheung <DanielHCheung@outlook.com>
2026-06-23 19:05:02 -0400

    Update site and base URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://danieldata.com/testblog/",
-	base: "/",
+	site: "https://danieldata.com",
+	base: "/testblog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
